94eb3619e898f8271c4f07583a5f280f09f3a257
resteve <resteve@zikzakmedia.com>
2015-12-01 11:05:35 +0100

    Tests

--- tests/test_carrier_shipment_note.py
+++ tests/test_carrier_shipment_note.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of carrier_shipment_note module for Tryton.
+# This file is part of the carrier_shipment_note module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierShipmentNoteTestCase(unittest.TestCase):
+class CarrierShipmentNoteTestCase(ModuleTestCase):
     'Test Carrier Shipment Note module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('carrier_shipment_note')
-
-    def test0005views(self):
-        'Test views'
-        test_view('carrier_shipment_note')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'carrier_shipment_note'
 
 
 def suite():
