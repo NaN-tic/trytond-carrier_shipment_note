5326896ca528e6bdd272c32cefb802569f00517a
Raimon Esteve <raimonesteve@gmail.com>
2024-07-09 09:12:21 +0200

    Replace carrier_notes to carrier_note #168436

--- shipment.py
+++ shipment.py
@@ -3,15 +3,22 @@
 # the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-
-__all__ = ['ShipmentOut', 'ShipmentOutReturn']
+from trytond.pyson import Bool, Eval, Not, Equal
 
 
 class ShipmentOut(metaclass=PoolMeta):
     __name__ = 'stock.shipment.out'
-    carrier_note = fields.Text('Carrier Note')
+    carrier_note = fields.Text('Carrier Note',
+        states={
+            'readonly': Equal(Eval('state'), 'done'),
+            'invisible': ~Eval('carrier'),
+            })
 
 
 class ShipmentOutReturn(metaclass=PoolMeta):
     __name__ = 'stock.shipment.out.return'
-    carrier_note = fields.Text('Carrier Note')
+    carrier_note = fields.Text('Carrier Note',
+        states={
+            'readonly': Equal(Eval('state'), 'done'),
+            'invisible': ~Eval('carrier'),
+            })
