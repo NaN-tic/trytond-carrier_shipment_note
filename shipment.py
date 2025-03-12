8e5c4961b06edf94cf34bf1c72e96ab5cec82946
Raimon Esteve <raimonesteve@gmail.com>
2025-03-12 08:03:02 +0100

    pyflakes

--- shipment.py
+++ shipment.py
@@ -3,7 +3,7 @@
 # the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.pyson import Bool, Eval, Not, Equal
+from trytond.pyson import Eval, Equal
 
 
 class ShipmentOut(metaclass=PoolMeta):
